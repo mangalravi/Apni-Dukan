75fc15e0af6f55f74b0d369e69ee9ea07529a2dc
ravi_mangal <116349588+mangalravi@users.noreply.github.com>
2025-11-11 09:56:58 +0530

    api updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7000/api/v1",
+  baseURL: "https://apni-dukan-production.up.railway.app/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
